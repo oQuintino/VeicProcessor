5fac97c3b91d07007d1ccfe2856cbb5147e0a7d1
OQuintino <matheus.qs2004@aluno.ifsc.edu.br>
2025-06-26 11:58:53 -0300

    added check for the fortran executable

--- fortran/run.sh
+++ fortran/run.sh
@@ -2,6 +2,11 @@
 
 cd ../data/raw || exit 1
 
+if [ ! -f ../../fortran/build/emiss.exe ]; then
+    echo "❌ Executable not found. Run 'make' first."
+    exit 1
+fi
+
 "Runs emiss.exe from the build folder, but while being in data/raw (so the files are in the correct path)"
 ../../fortran/build/emiss.exe
 
