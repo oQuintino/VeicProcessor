d2eb375f124b814fdfd2400ee83312966c1bf0f0
oQuintino <matheus.quintinodasilva0@gmail.com>
2025-06-27 03:55:04 -0300

    fixed script comment

--- fortran/run.sh
+++ fortran/run.sh
@@ -7,7 +7,7 @@ if [ ! -f ../../fortran/build/emiss.exe ]; then
     exit 1
 fi
 
-"Runs emiss.exe from the build folder, but while being in data/raw (so the files are in the correct path)"
+# Runs emiss.exe from the build folder, but while being in data/raw (so the files are in the correct path)
 ../../fortran/build/emiss.exe
 
 cd - > /dev/null
